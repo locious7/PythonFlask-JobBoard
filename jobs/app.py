33d8f84ce7e0378535053df254de0ce058f65364
locious <locious7@protonmail.com>
2021-08-18 17:00:18 -0600

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, g
 import sqlite3
+from flask import Flask, render_template, g
+
 
 PATH = "db/jobs.sqlite"
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connection, g._connection = sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
 
